6cf93b78f95db86d107c57400e619279016bd2fe
PONTIVEROS CALZADO Antonio Rafael <ar.pontiveros@soprasteria.com>
2023-11-14 10:03:12 +0100

    refactor tests

--- prices-container/src/test/java/com/inditex/prices/PricesApiE2ETests.java
+++ prices-container/src/test/java/com/inditex/prices/PricesApiE2ETests.java
@@ -7,14 +7,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class PricesApiIntegrationTests {
+public class PricesApiE2ETests {
 
     @Autowired
     private MockMvc mockMvc;
